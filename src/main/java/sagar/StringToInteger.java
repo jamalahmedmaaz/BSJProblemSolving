201792c3b2c073d22fb5413483ddbf978723a075
sagarsingh101 <sagarsingh247@rediff.com>
2019-03-14 00:16:26 +0530

    Create StringToInteger.java

---
+++ src/main/java/sagar/StringToInteger.java
@@ -0,0 +1,31 @@
+class StringToInteger {
+    public int myAtoi(String str) {
+        int ans=0,i=0;
+        int limit=-Integer.MAX_VALUE;
+        boolean negative=false;
+        int multMin = limit/10;
+        while(i<str.length() && str.charAt(i)==' ') i++;
+        if(i<str.length()) {
+            if(str.charAt(i)=='-') {negative=true;i++;}
+            else if(str.charAt(i)=='+'){i++;}
+        }
+        while(i<str.length()) {
+            if(isNumercal(str.charAt(i))) {
+                int digit = str.charAt(i)-'0';
+                if(ans<multMin) {
+                return negative?Integer.MIN_VALUE:Integer.MAX_VALUE;
+                }
+                ans*=10;
+                 if(ans<limit+digit) {
+                   return negative?Integer.MIN_VALUE:Integer.MAX_VALUE;
+                 }
+                 ans-=digit;
+                i++;
+            } else {break;}
+        }
+        return negative?ans:-ans;
+}
+    public static boolean isNumercal(char c){
+        return (c>='0' && c<='9') ;
+    }
+}
